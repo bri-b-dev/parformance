33a026dcce9a09fe45edf3ae54ed4d1fa66685b9
bri-b-dev <brigitte_boehm@outlook.de>
2025-10-25 18:18:15 +0200

    fix storybook

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,14 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const isStorybook = process.env.STORYBOOK === 'true'
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        vueDevTools(),
+        // Exclude vue-devtools (and its inspect plugin) when running under Storybook
+        ...(!isStorybook ? [vueDevTools()] : []),
     ],
     resolve: {
         alias: {
